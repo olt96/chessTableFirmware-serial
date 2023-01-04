739008ceee6a6f1b2182feef70d5be99630e950b
olt96asc <oliver.trajkovski@asseco-see.mk>
2023-01-05 00:06:43 +0100

    refactor logic, move it away from isr

--- arm_driver/arm_driver.ino
+++ arm_driver/arm_driver.ino
@@ -5,13 +5,15 @@
 #define EXTENSION_SERVO_PIN PB9
 #define GRABBER_MAX 100
 #define GRABBER_MIN 12
+#define GRABBER_CLOSED  false
+#define GRABBER_OPEN  true 
 
 #define EXTENSION_SERVO_SPEED 150
 #define GRABBER_SERVO_SPEED 1000
 
 #define SERVO_LOWER_LIMIT 80
 #define SERVO_UPPER_LIMIT 180
-#define EXTENDED_ARM_THRESHOLD 25 
+#define EXTENDED_ARM_THRESHOLD 100 
 #define TOGGLE_STATE_THRESHOLD_US 900000
 
 
@@ -21,7 +23,7 @@ volatile int pulseWidth = 0;
 volatile int oldPulseWidth = 0;
 volatile long pulses = 0;
 volatile long extendedTime = 0;
-volatile int grabberExtensionAngle = GRABBER_MIN;
+volatile bool grabber_state = GRABBER_OPEN ;
 
 bool writtenFlag = false;
 ServoEasing extensionServo;
@@ -30,9 +32,9 @@ void setup()
 {
   pinMode(RC_PIN, INPUT_PULLUP);
   extensionServo.attach(EXTENSION_SERVO_PIN, SERVO_UPPER_LIMIT);
-  grabberServo.attach(GRABBER_SERVO_PIN, grabberExtensionAngle);
+  grabberServo.attach(GRABBER_SERVO_PIN, GRABBER_MIN);
   extensionServo.write(SERVO_UPPER_LIMIT);
-  grabberServo.write(grabberExtensionAngle);
+  grabberServo.write(GRABBER_MIN);
 
   extensionServo.setSpeed(EXTENSION_SERVO_SPEED);
   grabberServo.setSpeed(GRABBER_SERVO_SPEED);
@@ -53,31 +55,10 @@ void PulseTimer()
   else
   {
     pulses += currentTime - startTime;
-    extendedTime += pulses;
     pulses = ((float)(currentTime - startTime) / pulses) * 100;
     if (pulses > 1)
     {
       pulseWidth = map(pulses, 0, 100, SERVO_LOWER_LIMIT, SERVO_UPPER_LIMIT);
-    }
-    if (pulses < EXTENDED_ARM_THRESHOLD)
-    {
-      if (extendedTime > TOGGLE_STATE_THRESHOLD_US && !writtenFlag)
-      {
-        if (grabberExtensionAngle == GRABBER_MAX)
-        {
-          grabberExtensionAngle = GRABBER_MIN;
-        }
-        else
-        {
-          grabberExtensionAngle = GRABBER_MAX;
-        }
-        writtenFlag = true;
-      }
-    }
-    else
-    {
-      extendedTime = 0;
-      writtenFlag = false;
     }
     startTime = currentTime;
   }
@@ -88,13 +69,19 @@ void loop()
 
   if (oldPulseWidth != pulseWidth)
   {
-    detachInterrupt(digitalPinToInterrupt(RC_PIN));
     extensionServo.easeTo(pulseWidth);
+    if (pulseWidth < EXTENDED_ARM_THRESHOLD) 
+    {
+      if (!writtenFlag){
+      grabber_state = !grabber_state ;
+      delay(100);
+      grabber_state? grabberServo.easeTo(GRABBER_MIN) : grabberServo.easeTo( GRABBER_MAX );
+      writtenFlag = true;
+      }
+    } else {
+      writtenFlag = false;
+    }
     oldPulseWidth = pulseWidth;
-    attachInterrupt(digitalPinToInterrupt(RC_PIN), PulseTimer, CHANGE);
-  }
-  if (writtenFlag)
-  {
-    grabberServo.write(grabberExtensionAngle);
-  }
+  } 
+  delay(100);
 }
